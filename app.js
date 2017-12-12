4344c3d7a55fa1deb3b97f974e2d9a3853aa9900
mrecker13 <mrecker@yahoo.com>
2017-12-12 12:41:13 -0800

    firebase and email added

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 $(window).on("load", function() {
     $(".modal-outer").fadeIn(1000);
-})
+});
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBAuahuC1FGJlDnYbTh_W4SNbyXxI4lDPs",
+    authDomain: "homepage-project-64ca7.firebaseapp.com",
+    databaseURL: "https://homepage-project-64ca7.firebaseio.com",
+    projectId: "homepage-project-64ca7",
+    storageBucket: "homepage-project-64ca7.appspot.com",
+    messagingSenderId: "438523083006"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
 
 $(document).ready(function() {
 
@@ -13,6 +26,8 @@ $(document).ready(function() {
 
         var name = $("#modalName").val();
         var loc = $("#modalLoc").val();
+        var email = $("#modalEmail").val();
+        var cleanEmail = email.replace(".", ",");
         var currentDate = moment().format("MMMM DD, YYYY");
         var currentTime = moment().format("hh:mm a");
 
@@ -20,5 +35,26 @@ $(document).ready(function() {
         $(".headerLocation").text(loc);
         $(".date").text(currentDate);
         $(".time").text(currentTime);
-    })
+
+        var user = {
+            name: name,
+            loc: loc,
+            email: cleanEmail
+        }
+
+        var userRef = database.ref().child(user.email);
+        userRef.set({
+            name: name,
+            loc: loc
+        })
+
+    });
+
+    // database.ref().on("value", function(snapshot) {
+    //     console.log(snapshot.val());
+
+        // $(".headerName").text(snapshot.val(name));
+        // $(".headerLocation").text(snapshot.val(loc));
+
+    // })
 });
