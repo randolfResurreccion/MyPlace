dcc22fd9a81f2c625bfb2ca88bc659654f31f1b2
chris <chrisb000@gmail.com>
2017-12-12 14:50:52 -0800

    help

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,73 @@
 
 
+// })
+
+var config = {
+    apiKey: "AIzaSyBAuahuC1FGJlDnYbTh_W4SNbyXxI4lDPs",
+    authDomain: "homepage-project-64ca7.firebaseapp.com",
+    databaseURL: "https://homepage-project-64ca7.firebaseio.com",
+    projectId: "homepage-project-64ca7",
+    storageBucket: "homepage-project-64ca7.appspot.com",
+    messagingSenderId: "438523083006"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
 $(document).ready(function () {
-    $(".modal-outer").fadeIn(1000);
+    $(".modal-outer-username").fadeIn(750);
+
+    $(".usernameNeed").click(function (event) {
+
+        event.preventDefault();
+
+        $(".modal-outer-username").hide();
+        $(".modal-inner-username").hide();
+        $(".modal-outer").slideToggle(750);
+        $(".modal-inner").slideToggle(750);
+
+    });
+
+    $(".usernameSubmit").click(function (event) {
+
+        event.preventDefault();
+
+        $(".modal-outer-username").fadeOut(1000);
+        $(".panel").show(750);
+
+        var unEmail = $("#usernameEmail").val().trim();
+        var cleanUnEmail = unEmail.replace(".", ",");
+
+
+
+        database.ref().child(cleanUnEmail).on("value", function (snapshot) {
+            console.log(snapshot.val());
+            var userName = snapshot.val().name;
+            console.log(userName);
+            var userLoc = snapshot.val().loc;
+            console.log(userLoc);
+            var currentDate = moment().format("MMMM DD, YYYY");
+            var currentTime = moment().format("hh:mm a");
+            weather.call(userLoc);
+            $(".headerName").text("Welcome, " + userName);
+            $(".date").text(currentDate);
+            $(".time").text(currentTime);
+
+        });
+    });
+
     $(".modalBtn").click(function (event) {
 
         event.preventDefault();
 
+        $(".modal-outer").fadeIn(1000);
         $(".modal-outer").hide(750);
         $(".panel").show(750);
 
         var name = $("#modalName").val();
         var loc = $("#modalLoc").val();
+        var email = $("#modalEmail").val();
+        var cleanEmail = email.replace(".", ",");
         var currentDate = moment().format("MMMM DD, YYYY");
         var currentTime = moment().format("hh:mm a");
 
@@ -18,5 +75,19 @@ $(document).ready(function () {
         $(".headerLocation").text(loc);
         $(".date").text(currentDate);
         $(".time").text(currentTime);
-    })
+
+        weather.call(loc);
+
+        var user = {
+            name: name,
+            loc: loc,
+            email: cleanEmail
+        }
+
+        var userRef = database.ref().child(user.email);
+        userRef.set({
+            name: name,
+            loc: loc
+        })
+    });
 });
