8d7cd7974340d08989c33aed88a63f55e70fdfea
mrecker13 <mrecker@yahoo.com>
2017-12-12 10:18:18 -0800

    index.html with modal

---
+++ app.js
@@ -0,0 +1,24 @@
+$(window).on("load", function() {
+    $(".modal-outer").fadeIn(1000);
+})
+
+$(document).ready(function() {
+
+    $(".modalBtn").click(function(event) {
+
+        event.preventDefault();
+
+        $(".modal-outer").hide(750);
+        $(".panel").show(750);
+
+        var name = $("#modalName").val();
+        var loc = $("#modalLoc").val();
+        var currentDate = moment().format("MMMM DD, YYYY");
+        var currentTime = moment().format("hh:mm a");
+
+        $(".headerName").text("Welcome, " + name);
+        $(".headerLocation").text(loc);
+        $(".date").text(currentDate);
+        $(".time").text(currentTime);
+    })
+});
