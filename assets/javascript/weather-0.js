a21dd8e4ee82ccc15c39628673b6a517c20a98b5
chris <chrisb000@gmail.com>
2017-12-12 11:11:21 -0800

    edits

--- assets/javascript/weather-0.js
+++ assets/javascript/weather-0.js
@@ -1,10 +1,17 @@
+
+console.log("test");
 weather = {
     call: (target) => {
+        let Url = "https://api.openweathermap.org/data/2.5/weather?zip=";
+        let search = "92130";
+        let key = "&appid=3e7cd0ac355b07dd223c526a716cd3f5";
         $.ajax({
-            url: Url,
+            url: Url + search + key,
             method: "GET"
         }).done(function(response){
             console.log(response);
         })
     }
 }
+
+weather.call();
